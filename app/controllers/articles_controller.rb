962b48e00cea645f860b6946b3987394d168ff07
Luis Novoa <luis.matte.novoa@gmail.com>
2019-12-28 22:25:44 -0400

    Implements delete button

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,12 +13,14 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    # @article = Article.new(
-    #   title: params[:article][:title],
-    #   body: params[:article][:body]
-    # )
     @article = Article.new(article_params)
     @article.save
     redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
   end
 end
